3dbf675755221282829addc06e2f119999bd79d7
lawrence <lawrenceo@activedgetechnologies.com>
2020-05-24 20:20:15 +0100

    added about.js and its nav item

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -1,11 +1,12 @@
-import React from "react"
-import {Link} from "gatsby"
-import ThemeChanger from "../components/themeChanger"
+import React from "react";
+import { Link } from "gatsby";
+import ThemeChanger from "../components/themeChanger";
 
 export default (props) => (
-  <nav className="navigation"> 
-    <Link to="/contact">Contact</Link>
-    <ThemeChanger/>
-  </nav>
-  
-)
+	<nav className='navigation'>
+		<Link to='/about'>About</Link>
+
+		<Link to='/contact'>Contact</Link>
+		<ThemeChanger />
+	</nav>
+);
